d8569bd5ec21617fec03522c1b4b1e72b3a5a172
nickzaro <nickzaro@gmail.com>
2020-11-02 12:34:59 -0300

    util implementado

---
+++ util/src/main/java/com/nick/microservices/util/http/GlobalControllerExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.nick.microservices.util.http;
+
+import com.nick.microservices.util.exceptions.InvalidInputException;
+import com.nick.microservices.util.exceptions.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
+
+public class GlobalControllerExceptionHandler {
+
+    @ResponseStatus(NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public @ResponseBody HttpErrorInfo handleNotFoundException(ServerHttpRequest request, Exception ex){
+        return createHttpErrorInfo(NOT_FOUND,request,ex);
+    }
+
+    @ResponseStatus(UNPROCESSABLE_ENTITY)
+    @ExceptionHandler(InvalidInputException.class)
+    public @ResponseBody HttpErrorInfo handleInvalidInputException(ServerHttpRequest request, Exception ex){
+        return createHttpErrorInfo(UNPROCESSABLE_ENTITY,request,ex);
+    }
+    private HttpErrorInfo createHttpErrorInfo(HttpStatus httpStatus,ServerHttpRequest request, Exception ex){
+            final String path = request.getPath().pathWithinApplication().value();
+            final String message = ex.getMessage();
+
+            return new HttpErrorInfo(httpStatus,path, message);
+    }
+}
