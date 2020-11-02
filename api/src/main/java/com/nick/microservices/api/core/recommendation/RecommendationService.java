8b2a0f2c4df3c2532405ebf4aebccf7962b12c71
nickzaro <nickzaro@gmail.com>
2020-11-02 19:44:21 -0300

    recommendation listo

--- api/src/main/java/com/nick/microservices/api/core/recommendation/RecommendationService.java
+++ api/src/main/java/com/nick/microservices/api/core/recommendation/RecommendationService.java
@@ -1,6 +1,7 @@
 package com.nick.microservices.api.core.recommendation;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -8,5 +9,5 @@ import java.util.List;
 public interface RecommendationService {
 
     @GetMapping(value = "/recommendation/{productId}", produces = "application/json")
-    List<Recommendation> getRecommendations(@RequestParam(value = "productId", required = true) int productId);
+    List<Recommendation> getRecommendations(@PathVariable(value = "productId", required = true) int productId);
 }
--- api/src/main/java/com/nick/microservices/api/core/review/ReviewService.java
+++ api/src/main/java/com/nick/microservices/api/core/review/ReviewService.java
@@ -1,6 +1,7 @@
 package com.nick.microservices.api.core.review;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -8,5 +9,5 @@ import java.util.List;
 public interface ReviewService {
 
     @GetMapping(value = "/review/{productId}", produces = "application/json")
-    List<Review> getReviews(@RequestParam(value = "productId", required = true) int productId);
+    List<Review> getReviews(@PathVariable(value = "productId", required = true) int productId);
 }
