03d6a3c63d47c610e99e0119d7ec574ec6656bf7
nickzaro <nickzaro@gmail.com>
2020-11-05 11:37:08 -0300

    Agregando swagger2 para la documentacion en product-composite-service

--- api/src/main/java/com/nick/microservices/api/composite/ProductCompositeService.java
+++ api/src/main/java/com/nick/microservices/api/composite/ProductCompositeService.java
@@ -1,10 +1,24 @@
 package com.nick.microservices.api.composite;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+@Api(description = "REST API for composite product information")
 public interface ProductCompositeService {
 
+    @ApiOperation(value = "${api.product-composite.get-composite-product.description}",
+    notes = "${api.product-composite.get-composite-product.notes}")
+
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code=404, message = "Not Found"),
+            @ApiResponse(code=422, message = "Unprocessable entity"),
+    }
+    )
     @GetMapping(value = "/product-composite/{productId}", produces = "application/json")
     ProductAggregate getProduct(@PathVariable int productId);
 }
--- microservices/product-composite-service/src/main/java/com/nick/microservices/composite/product/ProductCompositeServiceApplication.java
+++ microservices/product-composite-service/src/main/java/com/nick/microservices/composite/product/ProductCompositeServiceApplication.java
@@ -1,14 +1,58 @@
 package com.nick.microservices.composite.product;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.ResponseBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+import static org.springframework.http.HttpMethod.GET;
+import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
+import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
 
 @SpringBootApplication
 @ComponentScan("com.nick")
+@EnableSwagger2
 public class ProductCompositeServiceApplication {
+
+	@Value("${api.common.version}") String apiVersion;
+	@Value("${api.common.title}") String apiTitle;
+	@Value("${api.common.description}") String apiDescription;
+	@Value("${api.common.termsOfServiceUrl}") String apiTermsOfServiceUrl;
+	@Value("${api.common.license}") String apiLicense;
+	@Value("${api.common.licenseUrl}") String apiLicenseUrl;
+	@Value("${api.common.contact.name}") String apiContactName;
+	@Value("${api.common.contact.url}") String apiContactUrl;
+	@Value("${api.common.contact.email}") String apiContactEmail;
+
+	@Bean
+	public Docket apiDocumentation(){
+		return new Docket(SWAGGER_2)
+				.select()
+				.apis(basePackage("com.nick.microservices.composite.product"))
+				.paths(PathSelectors.any())
+				.build()
+				.globalResponses(GET, Collections.emptyList())
+				.apiInfo(new ApiInfo(
+								apiTitle,
+								apiDescription,
+								apiVersion,
+								apiTermsOfServiceUrl,
+								new Contact(apiContactName, apiContactUrl, apiContactEmail),
+								apiLicense,
+								apiLicenseUrl, Collections.emptyList()
+						)
+				);
+	}
 
 	@Bean
 	RestTemplate restTemplate(){
@@ -19,3 +63,4 @@ public class ProductCompositeServiceApplication {
 	}
 
 }
+
